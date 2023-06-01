40b7004e6c26b9cafe8aeaf1fe3cbb61d32faba1
Juan Silva <juansesilva245@gmail.com>
2023-06-01 10:57:10 -0500

    se agrego la linea 5

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 console.log("Si eva no se hubiera comido la manzana");
-console.log("La vida fuera mugo mas buena y mas sa");
+console.log("La vida fuera mugo mas buena y mas sana");
 console.log("Pero como esa cabrona se comio la fruta");
 console.log("Por eso que hoy en dia hay mujeres tan putas");
+console.log("les gusta mostrar los muslos y las tetas");
