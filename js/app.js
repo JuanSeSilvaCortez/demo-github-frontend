4896dab5ad2b54893f25f238df751fc78ea7d892
Juan Silva <juansesilva245@gmail.com>
2023-06-01 10:51:05 -0500

    Mujeres talentosas

--- js/app.js
+++ js/app.js
@@ -1,2 +1,4 @@
-console.log("hello JS");
-console.log("new line");
+console.log("Si eva no se hubiera comido la manzana");
+console.log("La vida fuera mugo mas buena y mas sa");
+console.log("Pero como esa cabrona se comio la fruta");
+console.log("Por eso que hoy en dia hay mujeres tan putas");
